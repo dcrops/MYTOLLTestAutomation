637b09a902eca298c8ee2609b0dc307c9b03832b
Sheyan Rizfee <sheyan.rizfee@tollgroup.com>
2018-01-23 13:39:48 +1100

    Script Updates

---
+++ MYTOLLTestAutomation/src/test/java/bookAPickupE2ETests/TollCarrierIntermodalSpecialized_AddAddress.java
@@ -0,0 +1,75 @@
+package bookAPickupE2ETests;
+
+import GlobalActions.PageBase;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+import bookAPickupActions.BookAPickupActions;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import baseWebdriver.BaseWebdriver;
+import myTollHomePageActions.MyTollHomePageActions;
+import reviewYourPickupActions.ReviewYourPickupActions;
+
+
+public class TollCarrierIntermodalSpecialized_AddAddress {
+
+	public static Integer locationIndex = 4;
+	
+	
+	
+
+	@BeforeMethod
+	public void RunSetup() throws Exception {
+		BaseWebdriver.setUp();
+		MyTollHomePageActions.Login(BaseWebdriver.Username, BaseWebdriver.Password);
+		MyTollHomePageActions.ClickMenu();
+		MyTollHomePageActions.ClickBookAPIckupMenu();
+
+	}
+
+	@Test(groups = { "Shakeout Testing" })
+	@Parameters({ "TollCarrierIntermodalSpecialized", "ServiceExpress", "locationIndex", "ItemTemplateName",
+			"NumberOfItems", "Length", "Width", "Height", "Weight", "temperatureLow", "temperatureHigh",
+			"refrigerationRefNum", "bookInNum", "VendorNum", "palletSpace", "Destination", "specialIns" })
+
+	public void TollCarrierIntermodalSpecialized_AddAddress(String TollCarrier,
+			String ServiceExpress, Integer locationIndex, String ItemTemplateName, String Length,
+			String NumberOfItems, String Width, String Height, String Weight, String tempLow, String tempHigh,
+			String ref, String BookNo, String vendorNum, String palletSpace, String destination, String specialIns) {
+
+		BookAPickupActions.SelectIntermodalSpecializedCarrier(TollCarrier);
+		//Select Account
+		BookAPickupActions.SelectAccountNumber1();
+		PageBase.MaximumWaitForElementEnabled();
+		
+		//Add Address
+		BookAPickupActions.addAdderess();
+		BookAPickupActions.verfiyAddress();
+
+		
+	}
+	
+	
+
+	@AfterMethod
+	public void RunTearDown() throws Exception {
+		// BaseWebdriver.tearDown();
+		//BaseWebdriver.driver.quit();
+
+	}
+
+}
--- MYTOLLTestAutomation/src/test/java/rateEnquiryE2ETests/TollIntermodalSpecialisedTests.java
+++ MYTOLLTestAutomation/src/test/java/rateEnquiryE2ETests/TollIntermodalSpecialisedTests.java
@@ -260,6 +260,83 @@ public class TollIntermodalSpecialisedTests {
 		Reporter.log("---------------END OF TEST---------------");
 	}
 	
+	
+	@Test
+	@Parameters({"TollIntermodalSpecialised", "ServiceFCL","TNAccountNo","ItemTemplateName","TNBillingType","TNMode", "TNNumberOfItems","TNLength", "TNWidth", "TNHeight", "TNWeight","TNQtyType", "TNOriginSuburb","TNOriginPostCode", "TNDesSuburb", "TNDesPostCode"})
+	public void RateEnquiry_TollIntermodalSpecialised_E2ETest_TID_1052_Service_FCL(String Carrier, String Service,  String AccountNo, String ItemTemplateName, String BillingType, String Mode, String NumberOfItems, String Length, String Width,
+			String Height, String Weight, String QtyType, String OriginSuburb,String OriginPostCode, String DesSuburb, String DesPostCode) {
+		
+		RateEnquiryActions.valid = false;
+		RateEnquiryActions.EnterTollCarrier(Carrier);
+		RateEnquiryActions.EnterService(Service);
+		RateEnquiryActions.SelectMode(Mode);
+		RateEnquiryActions.EnterAccountNumberAndSelect(AccountNo);
+		
+		RateEnquiryActions.SelectOrigin(OriginSuburb, OriginPostCode);
+		
+		RateEnquiryActions.SelecDestination(DesSuburb, DesPostCode);
+		
+		RateEnquiryActions.EnterItem(ItemTemplateName);
+		PageBase.moveToElement(RateEnquiryActions.billingTypeTDF);
+		RateEnquiryActions.BillingType(BillingType);
+		
+		RateEnquiryActions.NumberOfItem(NumberOfItems);
+		RateEnquiryActions.QuantityTypeSelect(QtyType);	
+		
+		RateEnquiryActions.EnterLengthWidthHeightVolumeWeight(Length, Width, Height, Weight);
+		
+		//Check for Price and Continue to Shipment
+		RateEnquiryActions.ClickPriceNow();
+		RateEnquiryActions.ContinueCreateShipment();
+
+		//Verify Details on Shipment Page
+		PageBase.waitForElement(RateEnquiryActions.shipmentCarrierName, 10);
+		PageBase.verifyTextExistAttributeContains(RateEnquiryActions.shipmentCarrierName, Carrier);
+		PageBase.verifyTextExistAttribute(RateEnquiryActions.shipmentService, Service);
+		PageBase.verifyTextExistAttribute(RateEnquiryActions.shipmentAccountNo, AccountNo);
+		PageBase.verifyTextExist(RateEnquiryActions.ShipmentDimention, Length+"cm x "+Width+" cm x "+Height+"cm" );
+		PageBase.waitForPageLoadingEnd(RateEnquiryActions.PageLoadingBox, 15, "Create Shipment");
+		Reporter.log("---------------END OF TEST---------------");
+	}
+	
+	@Test
+	@Parameters({"TollIntermodalSpecialised", "ServiceDGFCL","TNAccountNo","ItemTemplateName","TNBillingType","TNMode", "TNNumberOfItems","TNLength", "TNWidth", "TNHeight", "TNWeight","TNQtyType", "TNOriginSuburb","TNOriginPostCode", "TNDesSuburb", "TNDesPostCode"})
+	public void RateEnquiry_TollIntermodalSpecialised_E2ETest_TID_1052_Service_DGFCL(String Carrier, String Service,  String AccountNo, String ItemTemplateName, String BillingType, String Mode, String NumberOfItems, String Length, String Width,
+			String Height, String Weight, String QtyType, String OriginSuburb,String OriginPostCode, String DesSuburb, String DesPostCode) {
+		
+		RateEnquiryActions.valid = false;
+		RateEnquiryActions.EnterTollCarrier(Carrier);
+		RateEnquiryActions.EnterService(Service);
+		RateEnquiryActions.SelectMode(Mode);
+		RateEnquiryActions.EnterAccountNumberAndSelect(AccountNo);
+		
+		RateEnquiryActions.SelectOrigin(OriginSuburb, OriginPostCode);
+		
+		RateEnquiryActions.SelecDestination(DesSuburb, DesPostCode);
+		
+		RateEnquiryActions.EnterItem(ItemTemplateName);
+		PageBase.moveToElement(RateEnquiryActions.billingTypeTDF);
+		RateEnquiryActions.BillingType(BillingType);
+		
+		RateEnquiryActions.NumberOfItem(NumberOfItems);
+		RateEnquiryActions.QuantityTypeSelect(QtyType);	
+		
+		RateEnquiryActions.EnterLengthWidthHeightVolumeWeight(Length, Width, Height, Weight);
+		
+		//Check for Price and Continue to Shipment
+		RateEnquiryActions.ClickPriceNow();
+		RateEnquiryActions.ContinueCreateShipment();
+
+		//Verify Details on Shipment Page
+		PageBase.waitForElement(RateEnquiryActions.shipmentCarrierName, 10);
+		PageBase.verifyTextExistAttributeContains(RateEnquiryActions.shipmentCarrierName, Carrier);
+		PageBase.verifyTextExistAttribute(RateEnquiryActions.shipmentService, Service);
+		PageBase.verifyTextExistAttribute(RateEnquiryActions.shipmentAccountNo, AccountNo);
+		PageBase.verifyTextExist(RateEnquiryActions.ShipmentDimention, Length+"cm x "+Width+" cm x "+Height+"cm" );
+		PageBase.waitForPageLoadingEnd(RateEnquiryActions.PageLoadingBox, 15, "Create Shipment");
+		Reporter.log("---------------END OF TEST---------------");
+	}
+	
 	@AfterMethod
 	public void RunTearDown() throws Exception
 	{
