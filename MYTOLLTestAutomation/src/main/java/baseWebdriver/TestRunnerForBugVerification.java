677fe7eb0e6c8c640b4fad3506a6f44046567365
3407 <3407@PC12256.dialoggroup.internal>
2018-08-24 09:43:04 +1000

    Nishant

--- MYTOLLTestAutomation/src/main/java/baseWebdriver/TestRunnerForBugVerification.java
+++ MYTOLLTestAutomation/src/main/java/baseWebdriver/TestRunnerForBugVerification.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.*;
 
 
 @CucumberOptions( strict=true
-        ,features = {"src/test/Resources/bugVerification/MYT_8628 Users are unable to view certain POD.feature",
+        ,features = { "src/test/Resources/bugVerification/MYT_8628 Users are unable to view certain POD.feature",
         		"src/test/Resources/bugVerification/MYT_6850 Add Ship to mode for Intermodal & Specialised.feature",
         		"src/test/Resources/bugVerification/MYT_6361 Primary Address change verification.feature",
         		"src/test/Resources/bugVerification/MYT_6523 Ready Time and close time check.feature",
