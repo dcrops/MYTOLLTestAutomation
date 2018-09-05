4bc4819c447271556939e580231f6a49f3e4b0be
3407 <3407@PC12256.dialoggroup.internal>
2018-09-05 14:58:10 +1000

    test runner for bug verification. Added 8628 in the bug verification folder.

--- MYTOLLTestAutomation/src/main/java/baseWebdriver/TestRunnerForBugVerification.java
+++ MYTOLLTestAutomation/src/main/java/baseWebdriver/TestRunnerForBugVerification.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.*;
 
 @CucumberOptions( strict=true
 
-        ,features = {/*"src/test/Resources/bugVerification/MYT_550_checking_references_with_spaces_can_be_searched.feature", 
+        ,features = {"src/test/Resources/bugVerification/MYT_550_checking_references_with_spaces_can_be_searched.feature", 
         		"src/test/Resources/bugVerification/MYT_6850 Add Ship to mode for Intermodal & Specialised.feature",
         		//"src/test/Resources/bugVerification/MYT_6361 Primary Address change verification.feature",
         		"src/test/Resources/bugVerification/MYT_9022_Spelling_Error_in_the_account_already_exists_message.feature",
@@ -16,8 +16,8 @@ import cucumber.api.testng.*;
          		"src/test/Resources/bugVerification/MYT_6200 BU Admin User cannot see the My Admin Menu.feature" ,
          		"src/test/Resources/bugVerification/MYT_6126_Rate_enquiry_disclaimer_content.feature",
          		"src/test/Resources/bugVerification/MYT_9027_Grammar_error_on_the_My_Returns_page.feature",
-        		"src/test/Resources/bugVerification/MYT_8628 Users are unable to view certain POD.feature"
-        		*/	"src/test/Resources/bugVerification/MYT_8887Shipment_International_Domestic_Addresses.feature"	}
+        		"src/test/Resources/bugVerification/MYT_8887Shipment_International_Domestic_Addresses.feature",
+         		"src/test/Resources/bugVerification/MYT_8628 Users are unable to view certain POD.feature"}
 		, glue= {"bugVerificationStepsDefinitions","commonStepsDefinitions","createShipmentStepDefinitions"}
 		, plugin= {"html:C:\\Source\\MYTOLLTestAutomation\\Reports\\cucumber-pretty",
 
